1eabbaa148b2478b696dd7b0d191c5983833c525
Alee Herasimiuk <aherasimiuk@frba.utn.edu.ar>
2022-09-03 13:13:44 -0300

    Add files via upload

--- DHT11_-__Temperature_Sensor.ino
+++ DHT11_-__Temperature_Sensor.ino
@@ -1,10 +1,13 @@
 #include <DHT.h>
+#include <math.h>
 
 #define DHT_PIN 6
 #define TIPO_DHT DHT11
 
-DHT dht(DHT_PIN,TIPO_DHT);
 
+DHT dht(DHT_PIN,TIPO_DHT);
+int lastThresholdMeasured = 0;
+float lastTempMeasured = 0;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -15,6 +18,18 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   float t = dht.readTemperature();
-  Serial.println(t);
+  if(t != lastTempMeasured){
+    Serial.print("Temperature changed: ");
+    Serial.println(t);
+    lastTempMeasured = t;  
+  }
+
+  float measuredThreshold = analogRead(A0);
+  if(abs(measuredThreshold - lastThresholdMeasured) > 30 ){
+    Serial.print("Threshold changed: ");  
+    Serial.println(measuredThreshold);
+  }
+  
+  
   delay(1000);
 }
