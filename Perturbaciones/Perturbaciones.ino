47eb61ef11644b2fb002ccfb2077539c3fb71b76
Ayax Santos <62360415+ayaxsantos@users.noreply.github.com>
2022-10-22 13:02:31 -0300

    Fix Perturbaciones

--- Perturbaciones/Perturbaciones.ino
+++ Perturbaciones/Perturbaciones.ino
@@ -1,29 +1,55 @@
-const unsigned int kp = 0;
+#define LED_PIN 3
+
+const unsigned int kp = 1;
 unsigned int realim = 0;
 bool input = false;
+int input_fix = 0;
+int response = 0;
 
 int error = 0;
 
+void ajustar_valor_referencia();
+
+void ajustar_valor_referencia()
+{
+  if(input == true)
+    input_fix = 1023;
+  else
+    input_fix = 0;
+}
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   
-  pinMode(3,OUTPUT);
+  pinMode(LED_PIN,OUTPUT);
   pinMode(8,INPUT);
-  pinMode(A1,INPUT);
+  pinMode(A2,INPUT);
+
+  input = digitalRead(8);
+  ajustar_valor_referencia();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  input = digitalRead(8);
-  realim = analogRead(A1);
 
-  error = input - realim
+  realim = analogRead(A2);                    
+
+  error = input_fix - realim;
+  response = kp * error;
 
   Serial.print("Valor de referencia: ");
   Serial.println(input);
 
   Serial.print("Valor de realimentacion: ");
   Serial.println(realim);
+
+  Serial.print("Valor de error: ");
+  Serial.println(error);  
+
+  analogWrite(LED_PIN,response);
+  Serial.print("Diferencia de potencial SALIDA: ");
+  Serial.println((response / 1023.0)*5);  
+  Serial.println(" ");
+  delay(500);
 }
